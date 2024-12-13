b6b3be2655dac4e89c763363143f89342eb6d662
Prajwal <praj007955@gmail.com>
2024-12-13 10:30:59 +0530

    OAuth demo practise

---
+++ src/main/java/RestAPI/OauthDemo.java
@@ -0,0 +1,32 @@
+package RestAPI;
+
+import io.restassured.path.json.JsonPath;
+import static io.restassured.RestAssured.given;
+public class OauthDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String response =given()
+						.formParams("client_id",
+								"692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+						.formParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+						.formParams("grant_type", "client_credentials")
+						.formParams("scope", "trust")
+						.when().log().all()
+						.post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").asString();
+		System.out.println(response);
+
+		JsonPath jsonPath = new JsonPath(response);
+		String accessToken = jsonPath.getString("access_token");
+		System.out.println(accessToken);
+
+		String response2 = given()
+				.queryParams("access_token", accessToken)
+				.when()
+				.get("https://rahulshettyacademy.com/oauthapi/getCourseDetails")
+				.asString();
+		System.out.println(response2);
+
+	}
+
+}
